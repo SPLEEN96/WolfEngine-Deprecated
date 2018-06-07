fe544399fc2dafde66dc9864739e9a1ab1c35ec9
SPLEEN <SPLEEN@FOORBAR>
2018-06-06 20:45:38 -0400

    Encapsulation of some PointLight constant values

--- WolfEngine/Src/Components/Lights/PointLight.h
+++ WolfEngine/Src/Components/Lights/PointLight.h
@@ -14,37 +14,49 @@ enum POINT_RANGE {
 	RANGE_100,
 	RANGE_600
 };
-const float POINT_RANGEVAL[]{
-	7,
-	20,
-	32,
-	50,
-	100,
-	600
+
+class PointLight;
+struct PointLightConstantValues {
+	friend class PointLight;
+private:
+	/* Constants used calculate the Inverse Square Law in the shaders */
+	/* Values taken from wiki.ogre3d.org/-Point+Light+Attenuation */
+	const Vector3f kCONST[6]{
+		Vector3f(1.0f, 0.7f, 1.8f),
+		Vector3f(1.0f, 0.22f, 0.20f),
+		Vector3f(1.0f, 0.14f, 0.07f),
+		Vector3f(1.0f, 0.09f, 0.032f),
+		Vector3f(1.0f, 0.045f, 0.0075f),
+		Vector3f(1.0f, 0.007f, 0.0002f)
+	};
+	/* Possible range values for a PointLight (used in the gui) */
+	const u16 RANGE[6]{
+		7,
+		20,
+		32,
+		50,
+		100,
+		600
+	};
 };
-const Vector3f POINT_kCONST[]{
-	Vector3f(1.0f, 0.7f, 1.8f),
-	Vector3f(1.0f, 0.22f, 0.20f),
-	Vector3f(1.0f, 0.14f, 0.07f),
-	Vector3f(1.0f, 0.09f, 0.032f),
-	Vector3f(1.0f, 0.045f, 0.0075f),
-	Vector3f(1.0f, 0.007f, 0.0002f)
-};
+
+
+
 
 
 class PointLight :public BaseLight {
 private:
 	PointLight() {}
 public:
-	PointLight(RenderingEngineData* rdata, Vector3f color, float intensity, const POINT_RANGE &range)
+	PointLight(RenderingEngineData* rdata, Vector3f color, float intensity = 1.f, const POINT_RANGE &range = RANGE_7)
 		:BaseLight(color, intensity) {
 		m_light_type = LIGHT_POINT;
 
-		m_range = POINT_RANGEVAL[range];
+		m_range = m_values.RANGE[range];
 
-		m_kConstant = POINT_kCONST[range].X();
-		m_kLinear = POINT_kCONST[range].Y();
-		m_kQuadratic = POINT_kCONST[range].Z();
+		m_kConstant = m_values.kCONST[range].X();
+		m_kLinear = m_values.kCONST[range].Y();
+		m_kQuadratic = m_values.kCONST[range].Z();
 	
 		m_shader = &ForwardPointLight::GetInstance();
 		
@@ -52,10 +64,10 @@ public:
 	}
 
 	void SetRange(const POINT_RANGE &range) {
-		m_range = POINT_RANGEVAL[range];
-		m_kConstant = POINT_kCONST[range].X();
-		m_kLinear = POINT_kCONST[range].Y();
-		m_kQuadratic = POINT_kCONST[range].Z();
+		m_range = m_values.RANGE[range];
+		m_kConstant = m_values.kCONST[range].X();
+		m_kLinear = m_values.kCONST[range].Y();
+		m_kQuadratic = m_values.kCONST[range].Z();
 	}
 
 	Shader* GetShader() {
@@ -66,4 +78,5 @@ public:
 private:
 	float m_range;
 	float m_kConstant, m_kLinear, m_kQuadratic;
+	PointLightConstantValues m_values;
 };
