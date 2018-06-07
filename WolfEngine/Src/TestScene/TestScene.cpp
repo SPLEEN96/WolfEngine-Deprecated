8e9a39f705826546b4fa48fd971b4766cf651cc2
SPLEEN <SPLEEN@FOORBAR>
2018-06-06 20:47:19 -0400

    Setup of TestScene

--- WolfEngine/Src/TestScene/TestScene.cpp
+++ WolfEngine/Src/TestScene/TestScene.cpp
@@ -1,11 +1,51 @@
 #include "TestScene.h"
 
-
-#include "..\Core\ResourceManagement\ResourceFactory.h"
-//#include "..\Components\Camera.h"
-
 void TestScene::Init(ResourceFactory* rfactory_handle) {
+	/* Load some 3D models */
+	rfactory_handle->LoadModelDataInMemory("cube.3ds");
+	rfactory_handle->LoadModelDataInMemory("Wolf.obj");
 
+
+
+	/* Create a plane Entity with the cubel.3ds Model loaded*/
+	Entity *plane = rfactory_handle->BuildEntityFromModelData("cube.3ds", &MAT_EMERALD, "plane", false);
+	plane->GetTransform()->Scale(Vector3f(6.f, 0.1f, 10.f));             /* Scale it to make it look like a Plane*/
+	plane->GetTransform()->SetPosition(Vector3f(0.f, -1.3f, 0.f));
+
+	/* Create a Wolf Model Entity */
+	Entity *model = rfactory_handle->BuildEntityFromModelData("Wolf.obj", &MAT_WHITE, "Wolf", false);
+
+	/* Create a DirectionalLight */
+	Entity *sunlight = new Entity("sun", false);
+	sunlight->GetTransform()->SetPosition(Vector3f(0.f, 5.f, -5.f));
+	sunlight->AddComponent(new DirectionalLight(RenderingEngine::GetInstance().GetDataHandle(), Vector3f(0.5f), 0.1f, Vector3f(0.f, 0.f, 1.f)));
+
+	/* Create a Blue PointLight */
+	Entity *light_01 = new Entity("light_01", false);
+	light_01->GetTransform()->SetPosition(Vector3f(0.f, 0.f, 2.f));
+	light_01->AddComponent(new PointLight(RenderingEngine::GetInstance().GetDataHandle(), Vector3f(0.f, 0.f, 1.f), 1.f, RANGE_7));
+	light_01->AddComponent(new LightMeshRenderer((PointLight*)(light_01->GetComponent(0))));
+
+	/* Create a Red PointLight */
+	Entity *light_02 = new Entity("light_02", false);
+	light_02->GetTransform()->SetPosition(Vector3f(-0.8f, -1.f, 0.f));
+	light_02->AddComponent(new PointLight(RenderingEngine::GetInstance().GetDataHandle(), Vector3f(1.f, 0.f, 0.f), RANGE_20));
+	light_02->AddComponent(new LightMeshRenderer(((PointLight*)(light_02->GetComponent(0)))));
+
+	/* Create the Controller Entity with a Camera */
+	Entity* Controller = new Entity("Controller");
+	Controller->GetTransform()->SetPosition(Vector3f(0.f, 2.f, -3.5f));
+	Controller->AddComponent(new KeyboardMovement());
+	Controller->AddComponent(new Camera(RenderingEngine::GetInstance().GetDataHandle(), Controller, m_screen_width, m_screen_height));
+
+
+	/* Add all the entities to the root entity */
+	m_root_entity->AddChild(Controller);
+	m_root_entity->AddChild(sunlight);
+	m_root_entity->AddChild(plane);
+	m_root_entity->AddChild(model);
+	m_root_entity->AddChild(light_01);
+	m_root_entity->AddChild(light_02);
 }
 
 
