b9709d1d19e3496095ad12afe8ff27235172bbe6
SPLEEN <SPLEEN@FOORBAR>
2018-06-09 00:34:32 -0400

    Increased DynamicBuffer efficiency by reducing Realloc calls

--- WolfEngine/Src/Core/ResourceManagement/DataTypes/DynamicBuffer.h
+++ WolfEngine/Src/Core/ResourceManagement/DataTypes/DynamicBuffer.h
@@ -8,11 +8,12 @@ public:
 	DynamicBuffer() {
 		m_buffer = NULL;
 		m_len = 0;
+		m_capacity = 2;
 	}
 	~DynamicBuffer() {
 		if (m_len > 0)
 			free(m_buffer);
-		m_len = 0;
+		m_capacity = m_len = 0;
 	}
 
 	T& operator[](const u32& i) {
@@ -24,29 +25,28 @@ public:
 
 	const T& operator=(const T& rhs) {
 		this->m_len = rhs.m_len;
-		
+		this->m_capacity = rhs.m_capacity;
+
 		Clear(this->m_buffer);
 		memcpy(this->m_buffer, rhs.m_buffer, m_len);
 		
 		return *this;
 	}
 
+
 	void PushFront(const T& data) {
 		if (m_len > 0) {
 			m_len++;
-			T *tmp_buffer = NULL;
-			tmp_buffer = (T*)realloc(m_buffer, m_len * sizeof(T));
-			if (tmp_buffer) {
-				m_buffer = tmp_buffer;
-				memmove(&m_buffer[1], m_buffer, (m_len-1)*sizeof(T));
-				m_buffer[0] = data;
-			}
-			else
-				OutputErrorMsg("DynamicBuffer", ERR_MALLOC, "Failed to push data.");
+			if (m_len > m_capacity) 
+				AllocMem();
+			
+			memmove(&m_buffer[1], m_buffer, (m_len - 1) * sizeof(T));
+			m_buffer[0] = data;
 		}
 		else {
 			m_len++;
-			m_buffer = (T*)malloc(m_len * sizeof(T));
+			m_capacity++;
+			m_buffer = (T*)malloc(m_capacity * sizeof(T));
 			m_buffer[m_len - 1] = data;
 		}
 	}
@@ -55,12 +55,9 @@ public:
 		if (m_len > 1) {
 			m_len--;
 			memmove(&m_buffer[0], &m_buffer[1], (m_len) * sizeof(T));
-			T *tmp_buffer = NULL;
-			tmp_buffer = (T*)realloc(m_buffer, m_len * sizeof(T));
-			if (tmp_buffer)
-				m_buffer = tmp_buffer;
-			else
-				OutputErrorMsg("DynamicBuffer", ERR_MALLOC, "Failed to pop data.");
+
+			if ((float)(m_len*1.2f) < m_capacity)
+				FreeMem();
 		}
 		else if (m_len == 1)
 			Clear();
@@ -71,30 +68,24 @@ public:
 	void PushBack(const T& data) {
 		if (m_len > 0) {
 			m_len++;
-			T *tmp_buffer = NULL;
-			tmp_buffer = (T*)realloc(m_buffer, m_len * sizeof(T));
-			if (tmp_buffer) {
-				m_buffer = tmp_buffer;
-				m_buffer[m_len - 1] = data;
-			}
-			else
-				OutputErrorMsg("DynamicBuffer", ERR_MALLOC, "Failed to push back data.");
+			if (m_len >= m_capacity) 
+				AllocMem();
+			
+			m_buffer[m_len - 1] = data;
 		}
 		else {
 			m_len++;
-			m_buffer = (T*)malloc(m_len * sizeof(T));
+			m_capacity++;
+			m_buffer = (T*)malloc(m_capacity * sizeof(T));
 			m_buffer[m_len - 1] = data;
 		}
 	}
+
 	void PopBack() {
 		if (m_len > 1) {
 			m_len--;
-			T *tmp_buffer = NULL;
-			tmp_buffer = (T*)realloc(m_buffer, m_len * sizeof(T));
-			if (tmp_buffer)
-				m_buffer = tmp_buffer;
-			else
-				OutputErrorMsg("DynamicBuffer", ERR_MALLOC, "Failed to pop back data.");
+			if ((float)(m_len*1.2f) < m_capacity)
+				FreeMem();
 		}
 		else if (m_len == 1)
 			Clear();
@@ -105,6 +96,7 @@ public:
 	void Clear() {
 		if (m_len > 0) {
 			m_len = 0;
+			m_capacity = 2;
 			free(m_buffer);
 		}
 	}
@@ -116,6 +108,28 @@ public:
 		return m_len * sizeof(T);
 	}
 private:
+	void AllocMem() {
+		m_capacity *= 1.5f;
+		
+		T *tmp_buffer = NULL;
+		tmp_buffer = (T*)realloc(m_buffer, m_capacity * sizeof(T));
+		if (tmp_buffer)
+			m_buffer = tmp_buffer;
+		else
+			OutputErrorMsg("DynamicBuffer", ERR_MALLOC, "Failed to reallocate memory.");
+	}
+	void FreeMem() {
+		m_capacity /= 1.2f;
+
+		T *tmp_buffer = NULL;
+		tmp_buffer = (T*)realloc(m_buffer, m_capacity * sizeof(T));
+		if (tmp_buffer)
+			m_buffer = tmp_buffer;
+		else
+			OutputErrorMsg("DynamicBuffer", ERR_MALLOC, "Failed to free memory.");
+	}
+
 	T *m_buffer;
 	u32 m_len;
+	float m_capacity;
 };
